c4933a70bd8ecdcd41241de8b7dc172f90d39470
Aichi <82501311+curs3dst3ve@users.noreply.github.com>
2022-12-30 20:28:16 +0700

    Created main game folder.
    made main.py

---
+++ adv_game/main.py
@@ -0,0 +1,11 @@
+##############################
+# text adventure game
+##############################
+# variables
+na = "Adventurer"
+die = False
+insane_Effect = False
+Poisoned = False
+Health = 100
+##############################
+print("Hello Adventurer!")
