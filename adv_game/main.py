75a5a429c28f2f481acc698a66ef1bab1e39973d
Aichi <82501311+curs3dst3ve@users.noreply.github.com>
2023-03-02 18:57:08 +0700

    Update main.py

--- adv_game/main.py
+++ adv_game/main.py
@@ -17,12 +17,13 @@ secret_activator_2 = False
 secret_activator_3 = False
 ##############################
 print("\n")
-print("Hello! " + na +
-      ", This Text Adventure Game is open source and open for contribution.")
+print("Hello" + na +
+      ", this Text Adventure Game is open source and open for contribution.")
 print("If you are playing this game, please support or credit the creator please!")
 print("If you need help, or you want to contribute, can look at the game source code here: \nhttps://github.com/curs3dst3ve/text-adventure-game ")
 print("Alright then, " + na + " Let's get started!")
 na = str(input("To begin, what's you name?\n"))
+pronoun = input(
+    "Next, what's your pronouns? \n Select From \n He/Him \n She/Her \n or They/Them \n")
 print("Starting Game.\n")
 print("Hello " + na + "!")
-
