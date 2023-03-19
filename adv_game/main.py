a23e1148299246d337356c3a3f2c96c338e58481
Aichi <82501311+curs3dst3ve@users.noreply.github.com>
2023-03-19 16:01:38 +0700

    Update main.py

--- adv_game/main.py
+++ adv_game/main.py
@@ -5,6 +5,7 @@
 boypronouns = "he,him,mr,etc etc etc etc"
 girlpronouns = "she her mrs etc etc etc"
 theypronouns = "they them etc etc etc"
+difficulty = ""
 pronouns = ""
 na = ""
 die = False
@@ -22,11 +23,11 @@ secret_activator_3 = False
 ##############################
 print("\n")
 print("Hello" + na +
-      ", this Text Adventure Game is open source and open for contribution.")
-print("If you are playing this game, please support or credit the creator please!")
-print("If you need help, or you want to contribute, can look at the game source code here: \nhttps://github.com/curs3dst3ve/text-adventure-game ")
-print("Alright then, " + na + " Let's get started!")
-na = str(input("To begin, what's you name?\n"))
+      ", this Text Adventure Game is open source and open for contribution.\n")
+print("If you are playing this game, please support or credit the creator please!\n")
+print("If you need help, or you want to contribute, can look at the game source code here: \n                                https://github.com/curs3dst3ve/text-adventure-game \n")
+print("Alright then, " + na + " Let's get started! \n ")
+na = str(input("To begin, what's your name?\n"))
 pronoun = input(
     "Next, what's your pronouns? \n Select From \n He/Him \n She/Her \n or They/Them \n")
 if pronoun == "He/Him" or "he/him" or "he/Him" or "He/him":
@@ -37,5 +38,9 @@ elif pronoun == "He/Him" or "he/him" or "he/Him" or "He/him":
     pronoun = theypronouns
 else:
     print("Error 1: Pronoun Choosing.")
+
+difficulty = input(
+    "Then, please choose a difficulty.\n Easy\n Normal\n Hard\n Expert\n Master\n why the fuck would you choose this difficulty?\n")
+
 print("Starting Game.\n")
 print("Hello " + na + "!")
